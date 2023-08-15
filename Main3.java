94c6a1127fbf09155f90775d9a1b61e732212ea8
Leticia Mussolin <50248825+mussolin123@users.noreply.github.com>
2023-08-14 21:47:03 -0300

    matriz

---
+++ Main3.java
@@ -0,0 +1,43 @@
+class Main3{
+    public static void main(String[] args) {
+        int [] [] matriz;
+        int i, j;
+        int n_linhas = 7, n_colunas = 7;
+
+        matriz = new int [n_linhas] [n_colunas];
+
+            for(i=0;i<n_linhas;i++){
+                for(j=0;j<n_colunas;j++){
+                    if(i==0){
+                    matriz[i][j] = 1;
+                    
+                }else{
+
+                    if(i==n_linhas-1){
+                        matriz [i] [j] = 1;
+                    
+                    }else{
+                        if(j==0){
+                            matriz[i][j] = 1;
+                        }else{
+                            if(j==n_colunas -1){
+                                matriz[i][j] = 1;
+                            }else{
+                                matriz[i][j] = 9;
+                            }
+                        }                  
+                    }
+                }
+            }
+        }
+
+            for(i=0;i<n_linhas;i++){
+                for(j=0;j<n_colunas;j++){
+                    System.out.printf(" %d", matriz[i] [j]);
+        
+    }
+
+    System.out.println();
+        }
+    }
+}
